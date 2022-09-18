02757c6b408af4c92deca24d31506d5eeafd2528
Foklan Python <foklan@protonmail.com>
2022-09-18 22:40:33 +0200

    Add config

---
+++ config.py
@@ -0,0 +1,12 @@
+# KEYBOARD SHORTCUTS
+####################
+shortcut1 = "ctrl+shift+b"
+shortcut2 = "ctrl+shift+s"
+shortcut3 = "ctrl+shift+f"
+
+# Additional config
+###################
+# Duration between key detection cycles, lower
+# values will increase CPU usage, decrease only 
+# if shortcuts are not detected fast enough
+iter_sleep = 0.1
